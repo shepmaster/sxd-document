a6521e76cf15177abf0567036ffbe9ddcafcf0e9
Jake Goulding <jake.goulding@gmail.com>
2014-09-05 21:13:17 -0400

    Add some more helpers

--- src/parser.rs
+++ src/parser.rs
@@ -1,4 +1,4 @@
-use super::{Document,Root,RootChild};
+use super::{Document,Root,RootChild,Element};
 
 struct Parser;
 
@@ -7,41 +7,58 @@ impl Parser {
         Parser
     }
 
-    fn parse_preamble(&self, xml: &str) -> uint {
+    fn parse_preamble<'a>(&self, xml: &'a str) -> &'a str {
         // Parse the preamble
         let idx = xml.find_str("?>").expect("No preamble end");
         let end_of_preamble = idx + "?>".len();
-        end_of_preamble
+        xml.slice_from(end_of_preamble)
     }
 
-    fn parse_element<'a>(&self, xml: &'a str) -> (uint, &'a str) {
-        // Skip the opening
-        // TODO: verify is a <
-        let after_brace = "<".len();
-        let ele = xml.slice_from(after_brace);
-        let name = ele.slice_name().expect("failed to parse a name!");
-        (after_brace + name.len(), name)
+    fn parse_element<'a>(&self, doc: Document, xml: &'a str) -> (Element, &'a str) {
+        let (_, after_start_brace) = xml.expect_literal("<").expect("no start brace");
+
+        let (name, after_name) = after_start_brace.slice_name().expect("failed to parse a name!");
+
+        // skip_space
+        let (_, after_end_brace) = after_name.expect_literal(" />").expect("no end brace");
+
+        let e = doc.new_element(name.to_string());
+
+        (e, after_end_brace)
     }
 
     fn parse(&self, xml: &str) -> Document {
         let doc = Document::new();
 
-        let end_of_preamble = self.parse_preamble(xml);
-        let element = xml.slice_from(end_of_preamble);
-        let (_end_of_element, name) = self.parse_element(element);
+        let after_preamble = self.parse_preamble(xml);
 
-        let e = doc.new_element(name.to_string());
-        doc.root().append_child(e);
+        let (element, _tail) = self.parse_element(doc.clone(), after_preamble);
+        doc.root().append_child(element);
+
         doc
     }
 }
 
 trait XmlStr<'a> {
-    fn slice_name(&self) -> Option<&'a str>;
+    fn slice_at(&self, position: uint) -> (&'a str, &'a str);
+    fn expect_literal(&self, expected: &str) -> Option<(&'a str, &'a str)>;
+    fn slice_name(&self) -> Option<(&'a str, &'a str)>;
 }
 
 impl<'a> XmlStr<'a> for &'a str {
-    fn slice_name(&self) -> Option<&'a str> {
+    fn slice_at(&self, position: uint) -> (&'a str, &'a str) {
+        (self.slice_to(position), self.slice_from(position))
+    }
+
+    fn expect_literal(&self, expected: &str) -> Option<(&'a str, &'a str)> {
+        if self.starts_with(expected) {
+            Some(self.slice_at(expected.len()))
+        } else {
+            None
+        }
+    }
+
+    fn slice_name(&self) -> Option<(&'a str, &'a str)> {
         let mut positions = self.char_indices();
 
         let first_char = match positions.next() {
@@ -54,8 +71,8 @@ impl<'a> XmlStr<'a> for &'a str {
         let mut positions = positions.skip_while(|&(_, c)| c.is_name_char());
 
         match positions.next() {
-            Some((offset, _)) => Some(self.slice_to(offset)),
-            None => Some(self.clone()),
+            Some((offset, _)) => Some(self.slice_at(offset)),
+            None => Some((self.clone(), "")),
         }
     }
 }
