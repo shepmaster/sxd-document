f014dbc63ab164de018dde0f573901e1e1ab4d4f
Jake Goulding <jake.goulding@gmail.com>
2014-09-13 18:47:25 -0400

    Add a small utility to open and print out an XML file

---
+++ src/bin/open.rs
@@ -0,0 +1,40 @@
+extern crate document;
+
+use std::cmp::min;
+use std::io::File;
+
+use document::parser::Parser;
+
+fn pretty_error(xml: &str, position: uint) -> &str {
+    let s = xml.slice_from(position);
+    let l = s.char_len();
+    s.slice_chars(0, min(l, 15))
+}
+
+fn main() {
+    let mut args = std::os::args();
+
+    let filename = args.remove(1).expect("File required");
+    let path = Path::new(filename);
+    let mut file = File::open(&path);
+
+    let data = match file.read_to_end() {
+        Ok(x) => x,
+        Err(x) => fail!("Can't read: {}", x),
+    };
+
+    let data = match String::from_utf8(data) {
+        Ok(x) => x,
+        Err(x) => fail!("Unable to convert to UTF-8: {}", x),
+    };
+
+    let p = Parser::new();
+
+    let d = match p.parse(data.as_slice()) {
+        Ok(d) => d,
+        Err(point) => fail!("Unable to parse: {}", pretty_error(data.as_slice(), point)),
+    };
+
+    let mut out = std::io::stdio::stdout();
+    document::writer::format_document(&d, &mut out).ok().expect("I can't output");
+}
