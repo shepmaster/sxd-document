393a38b633f3e351c8a14d78b51729b6947a8c18
Jake Goulding <jake.goulding@gmail.com>
2014-09-13 17:13:28 -0400

    Write a single self-closed element with attributes

--- src/writer.rs
+++ src/writer.rs
@@ -1,6 +1,7 @@
 // Options
-// Space before /?
+// Space before /
 // Quote style
+// Ordering of attributes
 
 #[cfg(test)]
 mod test {
@@ -15,7 +16,11 @@ mod test {
     fn format_document<W : Writer>(doc: &Document, writer: &mut W) -> IoResult<()> {
         try!(writer.write_str("<?xml version='1.0'?>"));
         let e = doc.root().children()[0].element().expect("not an element?");
-        write!(writer, "<{}/>", e.name())
+        try!(write!(writer, "<{}", e.name()));
+        for attr in e.attributes().iter() {
+            try!(write!(writer, " {}='{}'", attr.name(), attr.value()));
+        }
+        writer.write_str("/>")
     }
 
     fn format_xml(doc: &Document) -> String {
@@ -25,12 +30,23 @@ mod test {
     }
 
     #[test]
-    fn format_an_element() {
+    fn top_element() {
         let d = Document::new();
         let e = d.new_element("hello".to_string());
         d.root().append_child(e);
 
         let xml = format_xml(&d);
         assert_str_eq!(xml, "<?xml version='1.0'?><hello/>");
+    }
+
+    #[test]
+    fn element_with_attributes() {
+        let d = Document::new();
+        let e = d.new_element("hello".to_string());
+        e.set_attribute("a".to_string(), "b".to_string());
+        d.root().append_child(e);
+
+        let xml = format_xml(&d);
+        assert_str_eq!(xml, "<?xml version='1.0'?><hello a='b'/>");
     }
 }
