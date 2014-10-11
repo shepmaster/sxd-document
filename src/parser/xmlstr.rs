0caa460bd4e5f155f96526750d59fb3aa3ae5c57
Jake Goulding <jake.goulding@gmail.com>
2014-10-11 11:26:39 -0400

    Update range syntax from Rust update

--- src/parser/xmlstr.rs
+++ src/parser/xmlstr.rs
@@ -187,22 +187,22 @@ trait XmlChar {
 impl XmlChar for char {
     fn is_name_start_char(&self) -> bool {
         match *self {
-            ':'                        |
-            'A'..'Z'                   |
-            '_'                        |
-            'a'..'z'                   |
-            '\U000000C0'..'\U000000D6' |
-            '\U000000D8'..'\U000000F6' |
-            '\U000000F8'..'\U000002FF' |
-            '\U00000370'..'\U0000037D' |
-            '\U0000037F'..'\U00001FFF' |
-            '\U0000200C'..'\U0000200D' |
-            '\U00002070'..'\U0000218F' |
-            '\U00002C00'..'\U00002FEF' |
-            '\U00003001'..'\U0000D7FF' |
-            '\U0000F900'..'\U0000FDCF' |
-            '\U0000FDF0'..'\U0000FFFD' |
-            '\U00010000'..'\U000EFFFF' => true,
+            ':'                         |
+            'A'...'Z'                   |
+            '_'                         |
+            'a'...'z'                   |
+            '\U000000C0'...'\U000000D6' |
+            '\U000000D8'...'\U000000F6' |
+            '\U000000F8'...'\U000002FF' |
+            '\U00000370'...'\U0000037D' |
+            '\U0000037F'...'\U00001FFF' |
+            '\U0000200C'...'\U0000200D' |
+            '\U00002070'...'\U0000218F' |
+            '\U00002C00'...'\U00002FEF' |
+            '\U00003001'...'\U0000D7FF' |
+            '\U0000F900'...'\U0000FDCF' |
+            '\U0000FDF0'...'\U0000FFFD' |
+            '\U00010000'...'\U000EFFFF' => true,
             _ => false,
         }
     }
@@ -210,12 +210,12 @@ impl XmlChar for char {
     fn is_name_char(&self) -> bool {
         if self.is_name_start_char() { return true; }
         match *self {
-            '-'                |
-            '.'                |
-            '0'..'9'           |
-            '\u00B7'           |
-            '\u0300'..'\u036F' |
-            '\u203F'..'\u2040' => true,
+            '-'                 |
+            '.'                 |
+            '0'...'9'           |
+            '\u00B7'            |
+            '\u0300'...'\u036F' |
+            '\u203F'...'\u2040' => true,
             _ => false
         }
     }
@@ -232,16 +232,16 @@ impl XmlChar for char {
 
     fn is_decimal_char(&self) -> bool {
         match *self {
-            '0'..'9' => true,
+            '0'...'9' => true,
             _ => false,
         }
     }
 
     fn is_hex_char(&self) -> bool {
         match *self {
-            '0'..'9' |
-            'a'..'f' |
-            'A'..'F' => true,
+            '0'...'9' |
+            'a'...'f' |
+            'A'...'F' => true,
             _ => false,
         }
     }
