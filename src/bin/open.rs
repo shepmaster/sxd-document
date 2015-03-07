aad0dd931d9a13a8ffac59b763e757573d50e74a
Jake Goulding <jake.goulding@gmail.com>
2015-03-07 15:29:23 -0500

    New IO system has stdin / stdout again

--- src/bin/open.rs
+++ src/bin/open.rs
@@ -2,15 +2,13 @@
 
 #![feature(collections)]
 #![feature(io)]
-#![feature(old_io)]
 
 extern crate document;
 
 use std::env;
 use std::cmp::min;
 use std::fs::File;
-use std::io::{Read,Cursor};
-use std::old_io::stdio;
+use std::io::{self,Read,Write};
 
 use document::parser::Parser;
 
@@ -25,10 +23,10 @@ fn process_input<R>(input: R)
 {
     let mut input = input;
     let mut data = String::new();
-    match input.read_to_string(&mut data) {
-        Ok(x) => x,
-        Err(x) => panic!("Can't read: {}", x),
-    };
+
+    if let Err(x) = input.read_to_string(&mut data) {
+        panic!("Can't read: {}", x);
+    }
 
     let p = Parser::new();
 
@@ -37,12 +35,12 @@ fn process_input<R>(input: R)
         Err(point) => panic!("Unable to parse: {}", pretty_error(&data, point)),
     };
 
-    // TODO: Remove buffering when std::io gets stdout
-    let mut out = Vec::new();
+    let mut out = io::stdout();
     let d = package.as_document();
-    document::writer::format_document(&d, &mut out).ok().expect("I can't output");
 
-    stdio::stdout_raw().write_all(&out).unwrap();
+    document::writer::format_document(&d, &mut out).ok().expect("I can't output");
+    // Remove when we move back to stdout_raw + buffer or when stdout flushed at program exit
+    out.flush().unwrap();
 }
 
 fn main() {
@@ -51,8 +49,7 @@ fn main() {
     let filename = args.remove(1);
 
     if filename == "-" {
-        let data = stdio::stdin_raw().read_to_end().unwrap();
-        process_input(Cursor::new(data));
+        process_input(io::stdin());
     } else {
         let file = File::open(&filename).unwrap();
         process_input(file);
